9c6bf6928194b41e9fbc5ce40b3c6fd0a9e04d4c
MKovblyuk <mkvblyuk@gmail.com>
2022-06-01 12:27:12 +0300

    change sidebar visual view

--- client/src/components/Content.js
+++ client/src/components/Content.js
@@ -1,13 +1,14 @@
 import React, { useContext } from "react";
 import {useContentRoutes} from "../contentRoutes"
 import { AuthContext } from "../context/AuthContext";
+import "../css/content.css"
 
 export const Content = () => {
     const {isAuthenticated} = useContext(AuthContext)
     const routes = useContentRoutes(isAuthenticated)
 
     return (
-        <div className="Content" style={{padding: "20px", width: "100%"}}>
+        <div className="Content">
             {routes}
         </div>
     )
--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,28 +1,43 @@
 import React from "react";
 import {NavLink} from "react-router-dom"
-import {Button, ButtonGroup} from '@mui/material';
+// import {Button, ButtonGroup} from '@mui/material';
 import "../css/sideBar.css"
+import profile_icon from "../images/sidebar/profile_icon.png"
+import skills_icon from "../images/sidebar/skills_icon.png"
+import tasks_icon from "../images/sidebar/tasks_icon.png"
+import todo_icon from "../images/sidebar/todo_icon.png"
 
 export const Sidebar = () => {
     return(
         <div className="Sidebar" style={{width: "300px"}}>
-            <ButtonGroup fullWidth={true} variant="outlined" size="large" aria-label="large button group" orientation="vertical">
+
                 <NavLink to="profile">
-                    <Button>Profile</Button>
+                    <button>
+                        <img src={profile_icon} alt="profile_icon"/>
+                        <div>Profile</div>
+                    </button>
                 </NavLink>
 
                 <NavLink to="skills">
-                    <Button>Skills</Button>
+                    <button>
+                        <img src={skills_icon} alt="skills_icon"/>
+                        <div>Skills</div>
+                    </button>
                 </NavLink>
 
                 <NavLink to="tasks">
-                    <Button>Tasks</Button>
+                    <button>
+                        <img src={tasks_icon} alt="tasks_icon"/>
+                        <div>Tasks</div>
+                    </button>
                 </NavLink>
 
                 <NavLink to="todo">
-                    <Button>ToDo</Button>
+                    <button>
+                        <img src={todo_icon} alt="todo_icon"/>
+                        <div>Todo</div>
+                    </button>
                 </NavLink>
-            </ButtonGroup>
         </div>
     )
 }
