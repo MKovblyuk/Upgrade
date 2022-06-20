ef6729396d7c97600eec50822f8290e645de79fe
MKovblyuk <mkvblyuk@gmail.com>
2022-06-20 12:55:30 +0300

    fix footer

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -2,10 +2,11 @@ import React from "react";
 import {Header} from "../components/Header"
 import {Footer} from "../components/Footer"
 import {Main} from "../components/Main"
+import "../css/pages/homePage.css"
 
 export const HomePage = () => {
     return (
-        <div>
+        <div className="HomePage">
             <Header/>
             <Main/>
             <Footer/>
